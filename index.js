af7903cd57134792a5ab5fa4903ed73488ee9efb
zsorosz <oroszi.zsofi@gmail.com>
2023-02-02 15:44:09 +0100

    Iteration-2 done

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ const data = require("./data");
 
 const MONGODB_URI = "mongodb://127.0.0.1/recipe-app";
 
+let AsianChicken = {
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs",
+  ],
+  cuisine: "Asian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu",
+};
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,7 +37,9 @@ mongoose
     return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create(AsianChicken)
+      .then((newRecipe) => console.log(newRecipe.title))
+      .catch((err) => console.log("oh nooo, errrrrorr", err));
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

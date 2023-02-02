661e6e474be800b91f9934c57463b25305d6c51e
zsorosz <oroszi.zsofi@gmail.com>
2023-02-02 15:56:35 +0100

    Iteration-3 done

--- index.js
+++ index.js
@@ -8,37 +8,25 @@ const data = require("./data");
 
 const MONGODB_URI = "mongodb://127.0.0.1/recipe-app";
 
-let AsianChicken = {
-  title: "Asian Glazed Chicken Thighs",
-  level: "Amateur Chef",
-  ingredients: [
-    "1/2 cup rice vinegar",
-    "5 tablespoons honey",
-    "1/3 cup soy sauce (such as Silver Swan®)",
-    "1/4 cup Asian (toasted) sesame oil",
-    "3 tablespoons Asian chili garlic sauce",
-    "3 tablespoons minced garlic",
-    "salt to taste",
-    "8 skinless, boneless chicken thighs",
-  ],
-  cuisine: "Asian",
-  dishType: "main_course",
-  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-  duration: 40,
-  creator: "Chef LePapu",
-};
-
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then((x) => {
+    // console.log(recipesArr);
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
   })
   .then(() => {
-    Recipe.create(AsianChicken)
-      .then((newRecipe) => console.log(newRecipe.title))
+    // Recipe.create(data[0])
+    //   .then((newRecipe) => console.log(newRecipe.title))
+    //   .catch((err) => console.log("oh nooo, errrrrorr", err));
+    Recipe.insertMany(data)
+      .then((allRecipes) => {
+        allRecipes.forEach((recipe) => {
+          console.log(recipe.title);
+        });
+      })
       .catch((err) => console.log("oh nooo, errrrrorr", err));
   })
   .catch((error) => {
